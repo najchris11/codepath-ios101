994ef755983b95b931a624858ca918e196c0bf2d
Najib <52837430+najchris11@users.noreply.github.com>
2025-07-01 20:38:20 -0400

    about to start week 5git add .

---
+++ ios101-lab3-cloudcast/ForecastViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ForecastViewController.swift
+//  CloudCast
+//
+//  Created by Christian Coulibaly on 6/18/25.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
