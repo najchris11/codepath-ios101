adb239eface5d45f6148169ff87e5a053d69cee3
Najib <52837430+najchris11@users.noreply.github.com>
2025-07-23 21:32:14 -0400

    trying to debug ios lab 7

--- ios101-project5-tumblr/ios101-project5-tumblr/TableViewCell.swift
+++ ios101-project5-tumblr/ios101-project5-tumblr/TableViewCell.swift
@@ -11,9 +11,22 @@ class TableViewCell: UITableViewCell {
     @IBOutlet weak var postPicture: UIImageView!
     @IBOutlet weak var postText: UILabel!
     
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        // Setup label
+        postText.numberOfLines = 0
+        postText.lineBreakMode = .byWordWrapping
+        postText.setContentHuggingPriority(.defaultLow, for: .horizontal)
+
+        // Setup image
+        postPicture.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        postPicture.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            postPicture.widthAnchor.constraint(equalToConstant: 100),
+            postPicture.heightAnchor.constraint(equalToConstant: 100)
+        ])
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
